cebc770feeec6d6a20daf9ec755b33156188b807
Casupang_KingJamcent <hyperking25@gmail.com>
2025-09-16 20:36:18 +0800

    Delete example1.py

--- example1.py
+++
@@ -1,9 +0,0 @@
-size = int (input("Enter Size: "))
-
-for x in range (1, size+1): 
-    for y in range(1, size+1):
-        if x%2==0:
-            print(f"*", end=" ")
-        else:
-            print(f"o", end=" ")
-    print()
